a2525790a41a8eb6d205dbfd7f6f61e9a61bc168
Naga Ramesh <rareddy@veracode.com>
2025-11-19 10:26:29 +0530

    Optimized for MAc

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,26 +1,35 @@
 import PyPDF2
 import pyttsx3
 
-# path of the PDF file
-path = open('concalls\\gls_feb2023.pdf', 'rb')
-
+# path of the PDF file (macOS compatible path)
+path = open('pdfs/gls_feb2023.pdf', 'rb')
 
 # creating a PdfFileReader object
 pdfReader = PyPDF2.PdfReader(path)
 
-start_page=2
-end_page=17
+# initialize text-to-speech once (more efficient)
+speak = pyttsx3.init()
+
+start_page = 2
+end_page = 17
+
+resp = 'y'
 
 while start_page < end_page:
+    if resp == 'c':
+        start_page += 1
+        continue
+    elif resp == 'y':
+        pass
+    elif resp == 'n':
+        break
+
     print(f"Reading page {start_page}")
     from_page = pdfReader.pages[start_page]
     text = from_page.extract_text()
-    speak = pyttsx3.init()
     speak.say(text)
     speak.runAndWait()
-    start_page+=1
-    resp = input("Want read the next page(y/n)?")
-    if resp == 'y':
-        pass
-    else:
-        break
+    start_page += 1
+    resp = input(f"Page {start_page} - Read(y)/Skip(c)/Quit(n)? ")
+
+path.close()
